326024d29de6ab568311087814ef7d084269008a
Nolan <nolanmmoss@gmail.com>
2024-03-31 16:15:16 -0400

    updated to fancy

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,10 +1,3 @@
-import faicons
-import shiny
-import pandas
-import plotly
-import scipy
-
-import shinywidgets
 # --------------------------------------------
 # Imports at the top - PyShiny EXPRESS VERSION
 # --------------------------------------------
@@ -12,129 +5,152 @@ import shinywidgets
 # From shiny, import just reactive and render
 from shiny import reactive, render
 
-# From shiny.express, import just ui
+# From shiny.express, import just ui and inputs if needed
 from shiny.express import ui
 
 # Imports from Python Standard Library to simulate live data
 import random
 from datetime import datetime
+from collections import deque
+
+# Import pandas for working with data
+import pandas as pd
 
 # --------------------------------------------
 # Import icons as you like
 # --------------------------------------------
+# add favicons to your requirements.txt 
+# and install to active project virtual environment
 
-from faicons import icon_svg
-
-# --------------------------------------------
-# FOR LOCAL DEVELOPMENT
-# --------------------------------------------
-
-
-# Add all packages not in the Std Library
-# to requirements.txt:
-#
-
-# And install them into an active project virtual environment (usually in .venv)
-# --------------------------------------------
+from faicons import icon_svg  
 
 
 # --------------------------------------------
-# SET UP THE REACIVE CONTENT
+# Shiny EXPRESS VERSION
 # --------------------------------------------
-
-# --------------------------------------------
-# PLANNING: We want to get a fake temperature and 
-# Time stamp every N seconds. 
-# For now, we'll avoid storage and just 
-# Try to get the fake live data working and sketch our app. 
-# We can do all that with one reactive calc.
-# Use constants for update interval so it's easy to modify.
-# ---------------------------------------------------------
 
 # --------------------------------------------
 # First, set a constant UPDATE INTERVAL for all live data
 # Constants are usually defined in uppercase letters
 # Use a type hint to make it clear that it's an integer (: int)
 # --------------------------------------------
+
 UPDATE_INTERVAL_SECS: int = 1
+
+# --------------------------------------------
+# Initialize a REACTIVE VALUE with a common data structure
+# The reactive value is used to store state (information)
+# Used by all the display components that show this live data.
+# This reactive value is a wrapper around a DEQUE of readings
 # --------------------------------------------
 
-# Initialize a REACTIVE CALC that our display components can call
+DEQUE_SIZE: int = 5
+reactive_value_wrapper = reactive.value(deque(maxlen=DEQUE_SIZE))
+
+
+# --------------------------------------------
+# Initialize a REACTIVE CALC that all display components can call
 # to get the latest data and display it.
 # The calculation is invalidated every UPDATE_INTERVAL_SECS
 # to trigger updates.
-
-# It returns everything needed to display the data.
+# It returns a tuple with everything needed to display the data.
 # Very easy to expand or modify.
-# (I originally looked at REACTIVE POLL, but this seems to work better.)
 # --------------------------------------------
 
 @reactive.calc()
 def reactive_calc_combined():
-
     # Invalidate this calculation every UPDATE_INTERVAL_SECS to trigger updates
     reactive.invalidate_later(UPDATE_INTERVAL_SECS)
 
-    # Data generation logic. Get random between -18 and -16 C, rounded to 1 decimal place
-    temp_c = round(random.uniform(-18, -16), 1)
+    # Data generation logic
+    temp = round(random.uniform(-18, -16), 1)
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    new_dictionary_entry = {"temp":temp, "timestamp":timestamp}
 
-    # Get a timestamp for "now" and use string format strftime() method to format it
-    timestamp_now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    # get the deque and append the new entry
+    reactive_value_wrapper.get().append(new_dictionary_entry)
 
-    latest_dictionary_entry = {"temp": temp_c, "timestamp": timestamp_now}
+    # Get a snapshot of the current deque for any further processing
+    deque_snapshot = reactive_value_wrapper.get()
 
-    # Return everything we need
-    return latest_dictionary_entry
+    # For Display: Convert deque to DataFrame for display
+    df = pd.DataFrame(deque_snapshot)
 
-# ------------------------------------------------
-# Define the Shiny UI Page layout - Page Options
-# ------------------------------------------------
+    # For Display: Get the latest dictionary entry
+    latest_dictionary_entry = new_dictionary_entry
 
+    # Return a tuple with everything we need
+    # Every time we call this function, we'll get all these values
+    return deque_snapshot, df, latest_dictionary_entry
+
+
+
+# Define the Shiny UI Page layout
 # Call the ui.page_opts() function
 # Set title to a string in quotes that will appear at the top
 # Set fillable to True to use the whole page width for the UI
 
-ui.page_opts(title="PyShiny Express: Live Temperature Data in Celsius (Basic)", fillable=True)
-
-# ------------------------------------------------
-# Define the Shiny UI Page layout - Sidebar
-# ------------------------------------------------
+ui.page_opts(title="PyShiny Express: Live Data With Value Card (Font Awesome Icon + 3 strings)", fillable=True)
 
 # Sidebar is typically used for user interaction/information
 # Note the with statement to create the sidebar followed by a colon
 # Everything in the sidebar is indented consistently
 
-
 with ui.sidebar(open="open"):
-    ui.h2("Antarctic Explorer", class_="text-center")
-    ui.p(
+
+  ui.h2("Antarctic Explorer", class_="text-center")
+  ui.p(
         "A demonstration of real-time temperature readings in Antarctica.",
         class_="text-center",
     )
+  ui.hr()
+  ui.h6("Links:")
+  ui.a(
+        "GitHub Source",
+        href="https://github.com/Crusoe22/cintel-05-cintel",
+        target="_blank",
+  )
+  ui.a(
+        "GitHub App",
+        href="https://Crusoe22.github.io/cintel-05-cintel/",
+        target="_blank",
+  )
+  ui.a("PyShiny", href="https://shiny.posit.co/py/", target="_blank")
 
-#---------------------------------------------------------------------
 # In Shiny Express, everything not in the sidebar is in the main panel
-#---------------------------------------------------------------------
+
+with ui.layout_columns():
+    with ui.value_box(
+        showcase=icon_svg("sun"),
+        theme="bg-gradient-blue-purple",
+    ):
+
+        "Current Temperature"
+
+        @render.text
+        def display_temp():
+            """Get the latest reading and return a temperature string"""
+            deque_snapshot, df, latest_dictionary_entry = reactive_calc_combined()
+            return f"{latest_dictionary_entry['temp']} C"
+
+        "warmer than usual"
+
+  
+
+    with ui.card(full_screen=True):
+        ui.card_header("Current Date and Time")
+
+        @render.text
+        def display_time():
+            """Get the latest reading and return a timestamp string"""
+            deque_snapshot, df, latest_dictionary_entry = reactive_calc_combined()
+            return f"{latest_dictionary_entry['timestamp']}"
 
 
-ui.h2("Current Temperature")
+with ui.layout_columns():
+    with ui.card():
+        ui.card_header("Current Data (placeholder only)")
 
-@render.text
-def display_temp():
-    """Get the latest reading and return a temperature string"""
-    latest_dictionary_entry = reactive_calc_combined()
-    return f"{latest_dictionary_entry['temp']} C"
-
-ui.p("Warmer than usual")
-    
-icon_svg("sun")
-
-ui.hr()
-
-ui.h2("Current Date and Time")
-
-@render.text
-def display_time():
-    """Get the latest reading and return a timestamp string"""
-    latest_dictionary_entry = reactive_calc_combined()
-    return f"{latest_dictionary_entry['timestamp']}"
+with ui.layout_columns():
+    with ui.card():
+        ui.card_header("Current Chart (placeholder only)")
